e579de741ab36bfcd58a102672e79264ef0d1769
Mayza <mayzamrodrigues@gmail.com>
2025-11-19 15:10:30 -0300

    chore: add new configurations to Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port: 3000,
+    host:'0.0.0.0'
+}, preview:{
+        port: 3000,
+        allowedHosts:[
+          'mayzaynara.com',],
 }
 })
